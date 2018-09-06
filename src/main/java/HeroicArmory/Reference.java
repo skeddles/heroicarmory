64e270e90797f69f8161844d20f773ac4cae1d70
skeddles <skeddtemp@gmail.com>
2018-09-06 19:18:45 -0400

    v1.1.3 fixes #1

--- src/main/java/HeroicArmory/Reference.java
+++ src/main/java/HeroicArmory/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MODID = "heroicarmory";
 	public static final String MODNAME = "Heroic Armory";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.3";
     public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
 	
 }
